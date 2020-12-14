b920fde48c35f306539d2d2b7971ba2f260d3c9c
ashrafomarcs <adming2010@gmail.com>
2020-12-14 12:01:31 +0300

    migration

---
+++ database/migrations/2020_12_14_084232_create_operation_records_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOperationRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operation_records', function (Blueprint $table) {
+            $table->id();
+            $table->longText('OprationDesc');
+            $table->string('status');
+            $table->string('depit');
+            $table->string('crdit');
+            $table->float('amount',8, 2);
+            $table->string('refer_no');
+            $table->longText('notes')->nullable();
+            
+            $table->unsignedBigInteger('oprationType')->nullable();
+            $table->foreign('oprationType')->references('id')->on('operation_types');
+
+            $table->unsignedBigInteger('wallet_id')->nullable();
+            $table->foreign('wallet_id')->references('id')->on('operation_records');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operation_records');
+    }
+}
