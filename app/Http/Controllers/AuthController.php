0ed0abaab6e35e08e1ec951d2b2bb547350fbf53
ashrafomarcs <adming2010@gmail.com>
2021-01-17 17:35:21 +0300

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,16 +28,17 @@ class AuthController extends Controller
      */
     public function login(Request $request){
     	$validator = Validator::make($request->all(), [
-            'email' => 'required|email',
+            'phone' => 'required',
             'password' => 'required|string|min:6',
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+            $res = array('status' =>false ,'error'=> $validator->errors());
+            return response()->json($res, 200);
         }
 
         if (! $token = auth()->attempt($validator->validated())) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['status'=>false,'error' => 'Unauthorized'], 200);
         }
 
         return $this->createNewToken($token);
@@ -59,7 +60,6 @@ class AuthController extends Controller
             'player_id'=>'required',
             'city_id'=>'required',
             'isSuspend'=>'required'
-
         ]);
 
         if($validator->fails()){
@@ -143,7 +143,8 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => auth()->user(),
+            'status' => true
         ]);
     }
 
